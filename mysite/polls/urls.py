dbf266929b6cabcfbdc97934f761bd889646a2a4
HanB312 <dlgksquf311@gmail.com>
2020-11-16 05:42:12 +0900

    web

---
+++ mysite/polls/urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 20:31:35 2020
+
+@author: SAMSUNG
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
